8259eef16fc78f20e9bff26c74835b1984ba6bb7
Leigh Halliday <leighhalliday@gmail.com>
2021-01-05 16:22:33 -0500

    Starting point

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
